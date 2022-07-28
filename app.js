4ca842516daa1eb70a86ba004f27ed7ce4135949
Sara Lutes <saralutes@gmail.com>
2022-07-28 16:02:13 -0500

    update server

--- app.js
+++ app.js
@@ -87,6 +87,6 @@ app.post("/return", function(req, res){
 })
 
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("server started on port 3000");
 });
